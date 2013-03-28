57557ccc09dbc82d3cf01c19ac0b7c4c050c9492
Benjamin Falk <benjaminfalk@Benjamins-MacBook-Pro.local>
2013-03-28 13:30:11 +0100

    Added shoppinglist project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoppinglist::Application.config.secret_token = '9ccda72a6f1f0125992d367add4393ebb00e3efc7459e7c95b2e29c3d89b3e2ee8e4c815d4409925477ef09f5d83807c9b9d98b388b3af975d06510b3ff5e136'
