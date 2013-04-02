204df35cfc87ece651f1994eaf7e1e4d36658e8c
Benjamin Falk <benjaminhf@hotmail.com>
2013-04-02 20:34:50 +0200

    trying to fix problem with have_selector, still not working

--- Gemfile
+++ Gemfile
@@ -7,10 +7,11 @@ gem 'rails', '3.2.13'
 
 group :development, :test do
     gem 'sqlite3'
-    gem "rspec-rails", "~> 2.0"
+    gem "rspec-rails", "~> 2.13.0"
+    gem "capybara", "2.0.3"
     gem "guard-rspec", "2.5.1"
     gem "rb-fsevent", "~> 0.9", :require => false
-    gem "growl", "1.0.3"
+   # gem "growl", "1.0.3"
 end
 group :production do
    gem 'pg'
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe HomeController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
+  describe "Home page" do
+    it "should have title 'Shoppinglist'" do
       get 'index'
-      response.should be_success
+      
+      response.should have_css("title", :text  => "Shoppinglist")
+     
     end
+#    it "should have the h1 'Sample App'" do
+ #     visit '/home/index'
+ # page.should have_selector('h1', :text => 'Sample App')
+#end
   end
+  
+  
 
 end
